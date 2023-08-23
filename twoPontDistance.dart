53cb29e0f599d6edfdf9594270f193a3699eef4a
Tareq Hossen <135951458+Tareq-Wasif@users.noreply.github.com>
2023-08-23 23:44:34 +0600

    Add files via upload

--- twoPontDistance.dart
+++ twoPontDistance.dart
@@ -1,19 +1,35 @@
-// Write a program to calculate distance between two points.
 import 'dart:io';
 import 'dart:math';
 
 void main() {
- 
-print("Enter the coordinates of two points.");
-  List<String> input1 = stdin.readLineSync()!.split(' ');
-  List<String> input2 = stdin.readLineSync()!.split(' ');
+  print("Enter the coordinates of two points (x1 y1 x2 y2):");
+  String input = stdin.readLineSync()!;
+  List<String> coordinates = input.split(' ');
 
-  double x1 = double.parse(input1[0]);
-  double y1 = double.parse(input1[1]);
-  double x2 = double.parse(input2[0]);
-  double y2 = double.parse(input2[1]);
+  // if (coordinates.length != 4) {
+  //   print("Invalid input. Please provide four coordinates.");
+  //   return;
+  // }
 
-  double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+  
+
+  double x1 = double.parse(coordinates[0]);
+  double y1 = double.parse(coordinates[1]);
+  double x2 = double.parse(coordinates[2]);
+  double y2 = double.parse(coordinates[3]);
+
+  double distance = calculateDistance(x1, y1, x2, y2);
 
   print("Distance: ${distance.toStringAsFixed(2)}");
 }
+
+double calculateDistance(double x1, double y1, double x2, double y2) {
+  double dx = x2 - x1;
+  double dy = y2 - y1;
+  double distance = sqrt(dx * dx + dy * dy);
+  return distance;
+}
+
+
+
+
